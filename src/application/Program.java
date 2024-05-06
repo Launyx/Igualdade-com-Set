d678cb86ff26399c5c4d331ed415352a358d5c0c
Launyx <lunyx.castelhano@gmail.com>
2024-05-06 14:03:41 -0300

    Utilizando TreeSet e implementando CompareTo

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,13 +2,16 @@ package application;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 import entities.Product;
 
 public class Program {
     public static void main(String[] args) {
         
-        Set<Product> set = new HashSet<>();
+
+        // Quando se usa TreeSet, a classe a ser comparada necessita implementar o CompareTo
+        Set<Product> set = new TreeSet<>();
 
         set.add(new Product("TV", 900.0));
         set.add(new Product("Notebook", 1200.0));
@@ -17,7 +20,12 @@ public class Program {
         Product prod = new Product("Notebook", 1200.0);
         
         // Antes das implementações de HashCode e equals na classe Product, o set compara o local de memória/ponteiro dos objetos
-        System.out.println(set.contains(prod));
+        //ystem.out.println(set.contains(prod));
         // Depois da implementação, o set passa a comparar os conteúdos determinados no HashCode e no equals
+
+        for (Product p : set){
+            System.out.println(p);
+        }
+
     }
 }
